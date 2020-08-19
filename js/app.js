06220c2974f3631cdf99cbdd4635369c4c78d9db
nyabuganda <jayp@nyabugandas-MacBook-Pro.local>
2020-08-18 21:44:29 -0600

    created conditions for variables

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var is_subscribed = false;
+var userAge=18;
+
+if(userAge > 18 && !is_subscrib) {
+    console.log("user is older than 18 and not subscrib");
+} 
+
+else if (userAge>=18 && !is_subscribed ) {
+    console.log("user is 18 and younger and not subscrib");
+}
+
+ else if (userAge<=18 && is_subscribed) {
+    console.log(" user is  younger than 18 and subscrib"); 
+}
+else if (userAge>=18 && is_subscribed) {
+    console.log("the user is 18 and older and is subscrib");
+}
+
+else  
+{
+    console.log(error);
+}
