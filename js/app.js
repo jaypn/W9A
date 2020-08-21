7c121cea66e096623709707e060d516bda78175b
nyabuganda <jayp@nyabugandas-MacBook-Pro.local>
2020-08-20 21:51:29 -0600

    w9b

--- js/app.js
+++ js/app.js
@@ -1,22 +1,40 @@
-var is_subscribed = false;
-var userAge=18;
 
-if(userAge > 18 && !is_subscrib) {
-    console.log("user is older than 18 and not subscrib");
-} 
 
-else if (userAge>=18 && !is_subscribed ) {
-    console.log("user is 18 and younger and not subscrib");
+var userAge =[17,19,16,20];
+
+var is_subscrib = [false,false,true,true];
+
+for (var index=0; index < userAge.length;index++) {
+    if((userAge[index] < 18) && (is_subscrib[index]==false)) {
+        console.log("user is younger than 18 and not subscrib");
+    } 
+    
+    else if ((userAge[index]>=18) && (is_subscrib[index]==false)) {
+        console.log("user is 18 and older and not subscrib");
+    }
+    
+     else if ((userAge[index]<18) && (is_subscrib[index]==true)) {
+        console.log("user is youger than 18 and  subscrib");
+    } 
+    
+    else if ((userAge[index]>=18) && (is_subscrib[index]==true)) {
+        console.log("user is 18 and older and  subscrib");
+    } 
+    else {
+        console.log("error");
+    }
+    console.log(index);
+
+    
+    
+    
 }
 
- else if (userAge<=18 && is_subscribed) {
-    console.log(" user is  younger than 18 and subscrib"); 
-}
-else if (userAge>=18 && is_subscribed) {
-    console.log("the user is 18 and older and is subscrib");
-}
 
-else  
-{
-    console.log(error);
-}
+
+
+
+
+
+
+
